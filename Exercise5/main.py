8d530a044a2d7a85bd58324bbec8f8300e628912
TymJanek <jantanski1@gmail.com>
2021-04-15 23:00:24 +0200

    added random seed improved project structure

--- Exercise5/main.py
+++ Exercise5/main.py
@@ -1,13 +1,15 @@
 import string
+import random
 
-
-from Decryptor import Decryptor
-from Encryptor import Encryptor
+from src.Decryptor import Decryptor
+from src.Encryptor import Encryptor
 
 
 if __name__ == '__main__':
     alphabet = list(string.ascii_lowercase)
     shuffled_alphabet = list(string.ascii_lowercase)
+    random.Random(4).shuffle(shuffled_alphabet)    # set seed for easier testing
+
     key = 8
 
     enc = Encryptor(alphabet, shuffled_alphabet, key)
@@ -17,7 +19,3 @@ if __name__ == '__main__':
     dec = Decryptor(alphabet, shuffled_alphabet, encryptor, key)
     decryptor = dec.decrypt()
     print('Decrypted text: \n', decryptor)
-
-
-
-
